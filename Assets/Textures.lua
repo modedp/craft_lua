f3487acda118f88c5cab65271339e2c3f3b79856
modedp <155116091+modedp@users.noreply.github.com>
2025-01-02 03:51:22 +0000

    Create Textures.lua

---
+++ Assets/Textures.lua
@@ -0,0 +1,60 @@
+
+
+return {
+	furnace = 5600979073,
+	litfurnace = 5601514105, 
+	torch = 3131912507,
+	["grasstop"] = 3131912045, -- Full grassy; Top texture of a grass block
+	["grassside"] = 3132023393, -- Dirt at bottom, some grass at top; Texture of sides of a grass block
+	["dirt"] = 3103457697, -- Full dirt; Bottom texture of a grass block
+
+	["stone"] = 3131912893, -- Full stone
+	["cobble"] = 3718806379,
+
+	["sand"] = 3725400827,
+
+	["bricks"] = 3479487392,
+
+	["glass"] = 3149393569, -- Transparent, often surrounded by white lines
+
+
+	["logside"] = 3149643651, -- Side of a log -- Known textures: 3595677229, 71445924
+	["logcut"] = 3149644490, -- Top/bottom of logs, similar to log texture
+	["planks"] = 3131912507, -- Fully planks
+
+	["leaves"] = 140507911, -- Fully leaves, semi-transparent
+
+
+
+	["coalblock"] = 150433747, -- Fully coal (dark color)
+	["ironblock"] = 59406020, -- Fully iron (platinum like color)
+	["diamondblock"] = 140574423, -- Fully diamond (cyan like color)
+
+
+	["obsidian"] = 3725380400, -- Fully obsidian
+
+
+	["bedrock"] = 3725398839, -- Darker texture of stone
+
+
+	["crafttop"] = 3725388039,
+	["craftside"] = 3725405589,
+	["craftfront"] = 3725408235,
+
+	["furnacetop"] = 3725389003, -- Top texture of the furnace, stone type
+	["furnaceside"] = 3725386392,
+
+
+	["diamondore"] = 3479489943, -- Diamonds (cyan-like color) stuck in a stone
+	["ironore"] = 3479499952, -- Iron nuggets (platinum-like color) stuck in a stone
+	["coalore"] = 3479489080, -- Coal stuck in a stone
+
+	-- Non-solids
+
+	["grass"] = 3829940143,
+
+
+	-- Liquid
+
+	["water"] = 204384984,
+};
