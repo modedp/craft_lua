313b985c8e858c9cfdf122926d0b3cd6b4a06024
modedp <155116091+modedp@users.noreply.github.com>
2025-01-02 03:51:01 +0000

    Create SmeltRecipes.lua

---
+++ Assets/SmeltRecipes.lua
@@ -0,0 +1,38 @@
+return {
+	
+	-- Smeltable
+	{
+		["Cobblestone"] = "Stone",
+		["Sand"] = "Glass",
+		["Clay"] = "Brick",
+		
+		["IronOre"] = "IronIngot",
+		["GoldOre"] = "GoldIngot",
+		
+		["DiamondOre"] = "Diamond",
+		["CoalOre"] = "Coal",
+		["RedstoneOre"] = "RedstoneDust",
+		
+		["OakLog"] = "Coal",
+	},
+	
+	-- Fuel
+	{
+		["LavaBucket"] = 20000,
+		
+		["CoalBlock"] = 16000,
+		["Coal"] = 1600,
+		
+		["OakPlanks"] = 300,
+		["OakLog"] = 300,
+		["CraftingTable"] = 300,
+		["Chest"] = 300,
+		
+		["WoodenPickaxe"] = 200,
+		["WoodenAxe"] = 200,
+		["WoodenSword"] = 200,
+		["WoodenShovel"] = 200,
+		
+		["Stick"] = 100,
+	}
+}
