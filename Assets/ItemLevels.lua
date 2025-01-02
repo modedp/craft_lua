a2a8c9d627c512d197bc64a872060a5956c081b3
modedp <155116091+modedp@users.noreply.github.com>
2025-01-02 03:50:31 +0000

    Create ItemLevels.lua

---
+++ Assets/ItemLevels.lua
@@ -0,0 +1,58 @@
+return {
+	hardnessBreak = {
+		shears = 4,
+		wooden = 2,
+		gold = 12,
+		stone = 4,
+		iron = 6,
+		diamond = 8,
+	},
+	shears = {
+		"shears"
+	},
+	shearsR = {
+		shears = 1,
+	},
+	pickaxe = {
+		"wooden",
+		"gold",
+		"stone",
+		"iron",
+		"diamond",
+	},
+	pickaxeR = {
+		wooden = 0,
+		gold = 1,
+		stone = 2,
+		iron = 3,
+		diamond = 4
+	},
+	axe = {
+		"wooden",
+		"gold",
+		"stone",
+		"iron",
+		"diamond",
+	},
+	axeR = {
+		wooden = 0,
+		gold = 1,
+		stone = 2,
+		iron = 3,
+		diamond = 4
+	},
+	shovel = {
+		"wooden",
+		"gold",
+		"stone",
+		"iron",
+		"diamond",
+	},
+	shovelR = {
+		wooden = 0,
+		gold = 1,
+		stone = 2,
+		iron = 3,
+		diamond = 4
+	},
+}
