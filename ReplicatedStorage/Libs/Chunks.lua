cce5ff1a7d4b99fcb0c4ae23cccbdb66d5c54719
modedp <155116091+modedp@users.noreply.github.com>
2025-05-01 22:49:37 +0000

    Update Chunks.lua

--- ReplicatedStorage/Libs/Chunks.lua
+++ ReplicatedStorage/Libs/Chunks.lua
@@ -1,7 +1,16 @@
-return function(p1, p2)
-	local v1 = {};
-	for v2 = 1, math.ceil(#p1 / p2) do
-		v1[v2] = p1:sub((v2 - 1) * p2 + 1, v2 * p2);
-	end;
-	return v1;
-end;
+-- Splits a string into chunks of specified size
+-- @param inputString The string to be split
+-- @param chunkSize The size of each chunk
+return function(inputString, chunkSize)
+    local chunks = {}
+    
+    -- Calculate number of chunks needed and split the string
+    for chunkIndex = 1, math.ceil(#inputString / chunkSize) do
+        -- Extract substring using the current chunk boundaries
+        local startPos = (chunkIndex - 1) * chunkSize + 1
+        local endPos = chunkIndex * chunkSize
+        chunks[chunkIndex] = inputString:sub(startPos, endPos)
+    end
+    
+    return chunks
+end
